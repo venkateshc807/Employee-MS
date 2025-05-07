34deb6749213d5bd35635a601684f18027df8fc4
venkateshc807 <cvenkatesh.bsc@gmail.com>
2025-05-07 16:31:11 +0530

    added skeleton loading effects

--- client/src/utils/RedirectBasedOnRole.jsx
+++ client/src/utils/RedirectBasedOnRole.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/authContext';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 
 const RedirectBasedOnRole = () => {
   const { user, loading } = useAuth();
@@ -18,7 +20,17 @@ const RedirectBasedOnRole = () => {
     }
   }, [user, loading, navigate]);
 
-  return <div>Loading...</div>;
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className="w-[300px]">
+        <Skeleton height={30} count={1} style={{ marginBottom: 12 }} />
+        <Skeleton height={20} count={2} />
+        <div className="mt-4">
+          <Skeleton height={40} width={100} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RedirectBasedOnRole;
--- client/src/utils/RoleBasedRoutes.jsx
+++ client/src/utils/RoleBasedRoutes.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../context/authContext';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 
 const RoleBasedRoutes = ({ children, requiredRole }) => {
   const { user, loading } = useAuth();
 
-  if (loading) return <div>Loading...</div>;
+  if (loading) {
+    return (
+      <div className="h-screen flex items-center justify-center">
+        <div className="w-[300px]">
+          <Skeleton height={30} count={1} style={{ marginBottom: 12 }} />
+          <Skeleton height={20} count={2} />
+          <div className="mt-4">
+            <Skeleton height={40} width={100} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   if (!requiredRole.includes(user?.role)) {
     return <Navigate to="/employee-dashboard" />;
